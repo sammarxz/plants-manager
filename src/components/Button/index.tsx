80bf6ac49f3aac66f1c3d774f183556c637de0bf
Sam Marxz <sammarxzz@gmail.com>
2023-08-12 16:30:18 -0300

    (feat) button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { styles } from "./styles";
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={styles.button}
+      activeOpacity={0.7} 
+      {...rest}
+    >
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+import colors from "../../styles/colors";
+
+export const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+  },
+});
